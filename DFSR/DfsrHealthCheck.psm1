f11347906fc971e9c0ad6b76029b3774e8855907
BonesUK <tonyroud@gmail.com>
2017-12-04 08:09:51 +0000

    Update functions and object output

--- DFSR/DfsrHealthCheck.psm1
+++ DFSR/DfsrHealthCheck.psm1
@@ -1,13 +1,125 @@
 <#
-To Do:
-Add a threshold parameter for Get-DfsrCriticalEvents to tweak how far back the monitor checks.
-Add a 'reset' parameter for Get-DfsrCriticalEvents to ignore anything before a specific time. 
-Add support for more than one DFSRDestination server
+    Module Name : DFSRHealthCheck
+    Author      : Tony Roud
+    Version     : 1.0
 
-Any issues with this module contact Tony Roud
+    To Do:
+    Add a 'reset' parameter for Get-DfsrCriticalEvents to ignore anything before a specific time. 
+    Add 'exclude' parameter for Replicated folders that don't need to me monitored
+    Add support for more than one DFSRDestination server
+    Include replicated folder status WMI info in each folder check
 #>
 
 # Region monitoring functions
+# Check for critical replication events in DFSR log (replication stopped on folder)
+# Threshold is set in hours (default 1)
+function Get-DfsrCriticalEvents {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]$threshold = 1
+    )
+    
+    [System.String]$status = '0'
+    $message = "No critical events found in DFSR log in the last $threshold hours."
+
+    Write-Verbose "Checking for critical events in the DFSR log within the last $threshold hours"
+    $startDate = ((Get-Date).addhours(-$threshold))
+    $event = Get-WinEvent -Filterhashtable @{LogName='DFS Replication';StartTime=$startDate;Level=2,1;ID='2104','4004'} -erroraction Silentlycontinue | Select-Object -first 1
+    
+    if ($event)
+    {
+        [System.String]$status = '2'
+        $message = "Warning - Critical DFSR replication events found in the last $threshold hours. Replication may have stopped for one or more replicated folders"
+        Write-Warning 'Warning - Critical DFSR replication events found in the last 60 mins. Replication may have stopped for one or more replicated folders'
+    }
+
+    [PSCustomObject]@{
+        'status'    = $status
+        'message'   = $message
+        'checkname' = 'DfsrCriticalEvents'
+    }
+}
+    
+# Basic health checks to see if WinRM and DFSR Services are running
+function Get-DfsrServiceStatus {
+    [CmdletBinding()]
+    Param()
+
+    [System.String]$status = '0'
+    $message = 'DFSR Service is running'
+
+    Write-Verbose 'Checking status of DFSR service'
+    if (!((Get-Service -Name DFSR).Status -eq 'Running')) {
+        [System.String]$status = '2'
+        $message = 'Warning. DFSR Service is stopped. Check DFSR service urgently.'
+        Write-Warning 'Warning. DFSR Service is stopped. Check DFSR service urgently.'
+    }
+    else 
+    {
+        Write-Verbose 'DFSR Service is running'
+    }
+    [PSCustomObject]@{
+        'message'   = $message
+        'checkname' = 'DfsrServiceStatus'
+        'status'    = $status 
+    }
+}
+
+function Get-WinRMServiceStatus {
+    [CmdletBinding()]
+    Param()
+
+    [System.String]$status = '0'
+    $message = 'WinRM Service is running'
+    
+    Write-Verbose 'Checking status of WinRM Service'
+    if (!((Get-Service -Name WinRm).Status -eq 'Running')) {
+        [System.String]$status = '1'
+        $message = 'Warning. WinRM Service is stopped. This may cause alerts for DFSR backlog checks.'
+        Write-Warning 'Warning. WinRM Service is stopped. This may cause alerts for DFSR backlog checks.'
+    }
+    else
+    {
+        Write-Verbose 'WinRM service is running'
+    }
+    [PSCustomObject]@{
+        'message'   = $message
+        'status'    = $status
+        'Checkname' = 'WinRMServiceStatus'
+    }
+}
+# Function to query WMI and return replication status code for replicated folder
+function Get-ReplicatedFolderState {
+    [CmdletBinding()]
+    [String[]]$warningFolders = @()
+    [System.String]$status = '1'
+    [System.String]$message = 'Unable to determine status for DFS replicated folders using WMI. Check DFSR health manually.'
+
+    Write-Verbose "Checking replicated folder status via WMI"
+    $replicatedfolders = Get-WmiObject -Namespace "root\Microsoft\Windows\DFSR" -Class msft_dfsrreplicatedfolderinfo | Select-Object replicatedfoldername,state
+    Foreach ($replicatedFolder in $replicatedfolders) 
+    {
+        If ($replicatedfolder.state -ne '4'){$warningFolders += $($replicatedfolder.name)}
+    }
+    if ($warningFolders.count -ge 1)
+    {
+        Write-Warning "Found one or more DFS replicated folders in abnormal state. Investigate DFSR replication health."
+        [System.String]$badfolders = $warningFolders | foreach-object {$_ + ','}
+        [System.String]$message = "Found one or more DFS replicated folders in abnormal state. Investigate DFSR replication health for folder(s): $badfolders"
+        $status = '2'
+    }
+    elseif ($warningFolders.count -lt 1)
+    {
+        Write-Verbose "WMI shows all DFS replicated folders are healthy."
+        $status = '0'
+        $message = 'All DFS replicated folders are healthy.'
+    }
+    [PSCustomObject]@{
+        'status'  = $status
+        'message' = $message
+    }
+
+}
 # Get a count of replication groups and replicated folders
 function Get-DfsrFolderInformation {
     [CmdletBinding()]
@@ -16,7 +128,7 @@ function Get-DfsrFolderInformation {
         )
 
     $connectionWarning = $false
-    $dfsrFolderInfo = Get-DfsReplicatedFolder -FolderName $replicatedFolder
+    $dfsrFolderInfo = Get-DfsReplicatedFolder -FolderName $replicatedFolder -Verbose
     
     try 
     {
@@ -29,89 +141,14 @@ function Get-DfsrFolderInformation {
         Write-Warning "Unable to get DFSR connection info for $replicatedFolder. Check DFSR health"
     }
 
-    $props = @{ 'DfsrGroup' = $dfsrFolderInfo.Groupname
-                'ReplicatedFolder' = $replicatedFolder
-                'DfsrSourceComputer' = $dfsrConnections.SourceComputerName
-                'DfsrDestinationComputer' = $dfsrConnections.DestinationComputerName
-                'ConnectionWarning' = $connectionWarning
+    [PSCustomObject]@{
+        'DfsrGroup'               = $dfsrFolderInfo.Groupname
+        'ReplicatedFolder'        = $replicatedFolder
+        'DfsrSourceComputer'      = $dfsrConnections.SourceComputerName
+        'DfsrDestinationComputer' = $dfsrConnections.DestinationComputerName
+        'ConnectionWarning'       = $connectionWarning
     }
-
-    New-Object -Typename PSObject -Property $props
 }
-
-# Check for critical replication events in DFSR log (replication stopped on folder)
-function Get-DfsrCriticalEvents {
-
-    $status = 0
-    $message = "No critical events found in DFSR log in the last 60 mins."
-
-    Write-Verbose "Checking for critical events in the DFSR log within the last 60 minutes"
-    $event = Get-EventLog -LogName "DFS Replication" -after ((Get-Date).addhours(-1)) | Where-Object { $_.message -match "DFS Replication service stopped replication on the replicated folder" }
-    if ($event)
-    {
-        $status = 1
-        $message = "Warning - Critical DFSR replication events found in the last 60 mins. Replication may have stopped for one or more replicated folders"
-        Write-Warning "Warning - Critical DFSR replication events found in the last 60 mins. Replication may have stopped for one or more replicated folders"
-    }
-    else 
-    {
-        Write-Verbose "No critical events found in DFSR log in the last 60 mins."
-    }
-    $props = @{
-        'status' = $status
-        'message' = $message
-    }
-
-    New-Object -TypeName PSObject -Property $props
-}
-
-# Simple checks to see if WinRM and DFSR Services are running
-function Get-DfsrServiceStatus {
-
-    $status = 0
-    $message = "DFSR Service is running"
-        
-    Write-Verbose "Checking status of DFSR service"
-    if (!((Get-Service -Name DFSR).Status -eq "Running")) {
-        $status = 2
-        $message = "Warning. DFSR Service is stopped. Check DFSR service urgently."
-        Write-Warning "Warning. DFSR Service is stopped. Check DFSR service urgently."
-    }
-    else 
-    {
-        Write-Verbose "DFSR Service is running"
-    }
-    $props = @{
-        'message'= $message
-        'status' = $status
-    }
-
-    New-Object -TypeName PSObject -Property $props 
-}
-
-function Get-WinRMServiceStatus {
-    
-    $status = 0
-    $message = "WinRM Service is running"
-    
-    Write-Verbose "Checking status of WinRM Service"
-    if (!((Get-Service -Name WinRm).Status -eq "Running")) {
-        $status = 1
-        $message = "Warning. WinRM Service is stopped. This may cause alerts for DFSR backlog checks."
-        Write-Warning "Warning. WinRM Service is stopped. This may cause alerts for DFSR backlog checks."
-    }
-    else
-    {
-        Write-Verbose "WinRM service is running"
-    }
-    $props = @{
-        'message' = $message
-        'status' = $status
-    }
-    
-    New-Object -TypeName PSObject -Property $props 
-}
-
 # Get DFSR Backlog count
 function Get-DfsrBacklogCount {
     [CmdletBinding()]
@@ -122,22 +159,22 @@ function Get-DfsrBacklogCount {
             [Parameter(Mandatory=$true)]$dfsrDestinationComputer
         )
 
-    $errorStatus = 0
+    [Bool]$errorStatus = $false
     $backlogCount = "N/A"
-    
+
     try
     {
         $backlogmsg = $( $null = Get-DfsrBacklog -GroupName $dfsrGroup -FolderName $replicatedFolder -SourceComputerName $dfsrSourceComputer -DestinationComputerName $dfsrDestinationComputer -Verbose -erroraction stop ) 4>&1
     }
     catch 
     {
-        Write-Warning "Unable to calculate backlog information, check DFSR Services are running"
-        $errorStatus = 1
+        Write-Warning 'Unable to calculate backlog information, check DFSR Services are running'
+        $errorStatus = $true
     }
 
-    if ($errorStatus -lt 1)
+    if (!$errorStatus)
     {    
-        if ($backlogmsg -match "No backlog for the replicated folder")
+        if ($backlogmsg -match 'No backlog for the replicated folder')
         {
             $backlogCount = 0
         }
@@ -150,17 +187,16 @@ function Get-DfsrBacklogCount {
             Catch
             {
                 Write-Warning "Unable to extract backlog count from Get-DfsrBacklog output. Manually check the command is returning data for $replicatedFolder."
-                $errorStatus = 1
+                $errorStatus = $true
             }
         }
     }
 
-    $props = @{
+    [PSCustomObject]@{
         'BacklogCount'= $backlogCount
-        'ErrorStatus'= $errorStatus
+        'ErrorStatus' = $errorStatus
+        'Checkname'   = "DFSRRepl_$replicatedFolder"
     }
-
-    New-Object -TypeName PSObject -Property $props 
 }
 # End region monitoring functions
 
@@ -174,74 +210,55 @@ function Get-DfsrHealthCheck {
             [Parameter(Mandatory=$true)]$CritThreshold
         )
 
-<#
-    $status = 1
-    $message = "Warning, unable to enumerate DFSR backlog for folder `"$folder`". Check DFSR services are running on server."
-#>
+    $checkName = $checkName = "DFSRRepl_$replicatedFolder"
     $dfsrReplicatedFolderInfo = Get-DfsrFolderInformation -replicatedFolder $folder
 
     if ($dfsrReplicatedFolderInfo.ConnectionWarning)
     {
-        $status = 2
+        [System.String]$status = '2'
         $message = "Unable to confirm connection details for folder $folder on $env:COMPUTERNAME. Check DFSR services are started."
         Write-Warning "Unable to confirm connection details for folder $folder on $env:COMPUTERNAME. Check DFSR services are started."
     }
     else
     {
-        $backlogCheck = Get-DfsrBacklogCount -replicatedFolder $folder -dfsrGroup $($dfsrReplicatedFolderInfo.DfsrGroup) -dfsrSourceComputer $env:COMPUTERNAME -dfsrDestinationComputer $($dfsrReplicatedFolderInfo.DfsrDestinationComputer)
-    
-        if ($backlogCheck.ErrorStatus -eq 1)
+        Write-Verbose "Checking for backlog for replicated folder $folder"
+        $backlogCheck = Get-DfsrBacklogCount -replicatedFolder $folder -dfsrGroup $($dfsrReplicatedFolderInfo.DfsrGroup) -dfsrSourceComputer $($dfsrReplicatedFolderInfo.DfsrSourceComputer) -dfsrDestinationComputer $($dfsrReplicatedFolderInfo.DfsrDestinationComputer) -Verbose
+
+        if ($backlogCheck.ErrorStatus)
         {
-            $status = 2
+            [System.String]$status = '2'
             $message = "Unable to calculate backlog for folder $folder on $env:COMPUTERNAME. Check DFSR services are started."
             Write-Warning "Unable to calculate backlog for folder $folder on $env:COMPUTERNAME. Check DFSR services are started."
         }
         elseif ( $backlogCheck.backlogCount -ge $critthreshold )
         {
-            $status = 2
+            [System.String]$status = '2'
             $message = "Backlog count for folder `"$folder`" in replication group `"$($dfsrReplicatedFolderInfo.DfsrGroup)`" is $($backlogCheck.backlogCount). Check DFSR replication health urgently."
             Write-warning "Backlog count for folder `"$folder`" in replication group `"$($dfsrReplicatedFolderInfo.DfsrGroup)`" is $($backlogCheck.backlogCount). Check DFSR replication health urgently."
         }
         elseif ( $backlogCheck.backlogCount -ge $warnThreshold )
         {
-            $status = 1
+            [System.String]$status = '1'
             $message = "Backlog count for folder `"$folder`" in replication group `"$($dfsrReplicatedFolderInfo.DfsrGroup)`" is $($backlogCheck.backlogCount). Check DFSR replication health."
             Write-Warning "Backlog count for folder `"$folder`" in replication group `"$($dfsrReplicatedFolderInfo.DfsrGroup)`" is $($backlogCheck.backlogCount). Check DFSR replication health."
         }
         elseif ( $backlogCheck.backlogCount -gt 0 )
         {
-            $status = 0
+            [System.String]$status = '0'
             $message = "Backlog count for folder `"$folder`" in replication group `"$($dfsrReplicatedFolderInfo.DfsrGroup)`" is $($backlogCheck.backlogCount)."
         }
         elseif ( $backlogCheck.backlogCount -eq 0 )
         {
-            $status = 0
+            [System.String]$status = '0'
             $message = "Backlog count for folder `"$folder`" in replication group `"$($dfsrReplicatedFolderInfo.DfsrGroup)`" is 0."
         }
     }
     
-    $props = @{
-        'status' = $status
-        'message' = $message
+    [PSCustomObject]@{
+        'status'    = $status
+        'message'   = $message
+        'Checkname' = $backlogCheck.checkName
     }
-    New-Object -TypeName PSObject -Property $props 
 }
-
-function Get-DfsrHealthCheckStatus {
-    [CmdletBinding()]
-        Param (
-            [Parameter(Mandatory=$true)]$replicatedfolder,
-            [Parameter(Mandatory=$true)]$warnThreshold,
-            [Parameter(Mandatory=$true)]$critthreshold
-        )
-    
-    # Perform healthcheck on replicated folder. 
-    Write-Verbose "Checking backlog for replicated folder $replicatedfolder"
-    $dfsrHealthcheck = Get-DfsrHealthCheck -folder $replicatedfolder -WarnThreshold $warnThreshold -CritThreshold $critthreshold
-    Write-Output "$($dfsrHealthcheck.status) DFSReplicationCheck_$replicatedfolder - $($dfsrHealthcheck.message)"
-    Write-Verbose "$($dfsrHealthcheck.status) DFSReplicationCheck_$replicatedfolder - $($dfsrHealthcheck.message)"
-
-}
-# End region DFSR Healthcheck function
-
-Export-ModuleMember -function Get-DfsrFolderInformation,Get-DfsrCriticalEvents,Get-DfsrServiceStatus,Get-WinRMServiceStatus,Get-DfsrBacklogCount,Get-DfsrHealthCheck,Get-DfsrHealthCheckStatus
+# End region DFSR Healthcheck functions
+Export-ModuleMember -function * 
